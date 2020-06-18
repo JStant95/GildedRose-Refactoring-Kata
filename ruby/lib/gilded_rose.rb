43219c0d287db7e1a26a60955ba801bc5b6ab4bb
Jed Stanton <jedstanton@hotmail.com>
2020-06-18 11:51:30 +0100

    Update sell in method added

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -1,20 +1,21 @@
 class GildedRose
-  attr_reader :normal_items, :conjured_items, :Aged_bries, :backstage_passes
+  attr_reader :normal_items, :conjured_items, :aged_bries, :backstage_passes
 
   def initialize(items)
     @items = items
   end
 
+
   def create_categories()
-    @normal_items = @items.select { |item| !item.name.include?("Aged Brie") and !item.name.include?("Backstage passes") and !item.name.include?("Sulfras") }
+    @normal_items = @items.select { |item| !item.name.include?("Aged Brie") and !item.name.include?("Backstage passes") and !item.name.include?("Sulfuras") }
     @conjured_items = @items.select { |item| item.name.include?("Conjured") }
-    @Aged_bries = @items.select { |item| item.name.include?("Aged Brie") }
+    @aged_bries = @items.select { |item| item.name.include?("Aged Brie") }
     @backstage_passes = @items.select { |item| item.name.include?("Backstage") }
   end
 
   def update_normal_items()
+    update_sell_in(@normal_items)
     @normal_items.map { |item| item.quality -= 1 if item.quality > 0 }
-    @normal_items.map { |item| item.sell_in -= 1 }
   end
 
   def update_conjured_items()
@@ -25,52 +26,28 @@ class GildedRose
   end
 
   def update_backstage_passes()
-    @backstage_passes.map { |item| item.quality += 1 if item.quality > 50 }
-    @backstage_passes.map { |item| item.quality += 1 if item.quality > 50 and item.sell_in <11 }
-    @backstage_passes.map { |item| item.quality += 1 if item.quality > 50 and item.sell_in <6 }
-    @backstage_passes.map { |item| item.quality = 0 if item.sell_in > 0 }
-    @backstage_passes.map { |item| item.sell_in -= 1 }
+    update_sell_in(@backstage_passes)
+    @backstage_passes.map { |item| item.quality += 1 if item.quality < 50 }
+    @backstage_passes.map { |item| item.quality += 1 if item.quality < 50 and item.sell_in < 11 }
+    @backstage_passes.map { |item| item.quality += 1 if item.quality < 50 and item.sell_in <6 }
+    @backstage_passes.map { |item| item.quality = 0 if item.sell_in < 0 }
   end
 
+  def update_aged_bries()
+    update_sell_in(@aged_bries)
+    @aged_bries.map { |item| item.quality += 1 if item.quality < 50 }
+  end
+
+  def update_sell_in(category)
+    category.map { |item| item.sell_in -= 1 }
+  end
 
   def update_quality()
-    @items.each do |item|
-      if !item.name.include?("Aged Brie") and !item.name.include?("Backstage passes")
-          if !item.name.include?("Sulfuras")
-            update_normal_item(item)
-          end
-          if item.name.include?("Conjured")
-            update_conjured_item(item)
-          end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name.include?("Backstage passes")
-            update_backstage_passes(item)
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+    create_categories()
+    update_normal_items()
+    update_conjured_items()
+    update_backstage_passes()
+    update_aged_bries()
   end
 end
 
--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -1,127 +1,6 @@
 require "gilded_rose"
 
 describe GildedRose do
-  # describe "#update_quality" do
-  #   it "A normal item changes quality by 1 after 1 day" do
-  #     items = [Item.new("Vest", 10, 10)]
-  #     GildedRose.new(items).update_quality()
-  #     expect(items[0].quality).to eq 9
-  #   end
-  #
-  #   it "A normal item changes sellIn by 1 after 1 day" do
-  #     items = [Item.new("Vest", 10, 10)]
-  #     GildedRose.new(items).update_quality()
-  #     expect(items[0].sell_in).to eq 9
-  #   end
-  #
-  #   it "A normal item changes quality and sellIn by 2 after 2 days" do
-  #     items = [Item.new("Vest", 10, 10)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     shop.update_quality()
-  #     expect(items[0].sell_in).to eq 8
-  #     expect(items[0].quality).to eq 8
-  #   end
-  #
-  #   it "the quality of a normal item does not go below zero" do
-  #     items = [Item.new("Vest", 10, 1)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 0
-  #   end
-  #
-  #   it "Aged brie increases in quality by 1 and decreases Sellin by 1 after 1 day" do
-  #     items = [Item.new("Aged Brie", 10, 10)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 11
-  #     expect(items[0].sell_in).to eq 9
-  #   end
-  #
-  #   it "Aged brie increases in quality by 2 and decreases Sellin by 2 after 2 day" do
-  #     items = [Item.new("Aged Brie", 10, 10)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 12
-  #     expect(items[0].sell_in).to eq 8
-  #   end
-  #
-  #   it "Aged brie quality cannot increase beyond 50" do
-  #     items = [Item.new("Aged Brie", 10, 50)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 50
-  #     expect(items[0].sell_in).to eq 9
-  #   end
-  #
-  #   it 'A sulfras quality and sellin value does not change' do
-  #     items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 80)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 80
-  #     expect(items[0].sell_in).to eq 10
-  #   end
-  #
-  #   it "A backstage pass quality increases by 1 each day when the sellIn value is above 10" do
-  #     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 49)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 50
-  #     expect(items[0].sell_in).to eq 10
-  #   end
-  #
-  #   it "A backstage pass quality increases by 2 each day when the sellIn value is between 10 & 5" do
-  #     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 9, 45)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 47
-  #     expect(items[0].sell_in).to eq 8
-  #   end
-  #
-  #   it "A backstage pass quality increases by 3 each day when the sellIn value is below 5" do
-  #     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 45)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 48
-  #     expect(items[0].sell_in).to eq 4
-  #   end
-  #
-  #   it "A backstage pass quality increases by 2 each day when the sellIn value is between 10 & 5" do
-  #     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 9, 45)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 47
-  #     expect(items[0].sell_in).to eq 8
-  #   end
-  #
-  #   it "A backstage pass quality cannot increase past 50" do
-  #     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 50)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 50
-  #     expect(items[0].sell_in).to eq 10
-  #   end
-  #
-  #   it "A backstage pass quality goes to 0 after its sellIn value has past" do
-  #     items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 49)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 0
-  #     expect(items[0].sell_in).to eq -1
-  #   end
-  #
-  #   it "A conjured item's value decreases by 2 after each day" do
-  #     items = [Item.new('Conjured bread', 10, 40)]
-  #     shop = GildedRose.new(items)
-  #     shop.update_quality()
-  #     expect(items[0].quality).to eq 38
-  #     expect(items[0].sell_in).to eq 9
-  #   end
-  # end
-
   describe "#create_categories" do
     it "Splits items into their appropiate category" do
       items = [Item.new("Vest", 10, 10), Item.new('Conjured bread', 10, 40), Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 49), Item.new("Aged Brie", 10, 50)]
@@ -130,7 +9,14 @@ describe GildedRose do
       expect(shop.normal_items[0].name).to eq("Vest")
       expect(shop.conjured_items[0].name).to eq("Conjured bread")
       expect(shop.backstage_passes[0].name).to eq("Backstage passes to a TAFKAL80ETC concert")
-      expect(shop.Aged_bries[0].name).to eq("Aged Brie")
+      expect(shop.aged_bries[0].name).to eq("Aged Brie")
+    end
+
+    it "Can deal with multiple items of the same category" do
+      items = [Item.new("Vest", 10, 10), Item.new('Shirt', 10, 40)]
+      shop = GildedRose.new(items)
+      shop.create_categories()
+      expect(shop.normal_items.length).to eq(2)
     end
   end
 
@@ -183,4 +69,90 @@ describe GildedRose do
       expect(items[0].sell_in).to eq 9
     end
   end
-end
+
+  describe "#update_backstage_passes" do
+    it "A backstage pass quality increases by 1 each day when the sellIn value is above 10" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 49)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_backstage_passes()
+        expect(items[0].quality).to eq 50
+        expect(items[0].sell_in).to eq 10
+      end
+
+      it "A backstage pass quality increases by 2 each day when the sellIn value is between 10 & 5" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 9, 45)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_backstage_passes()
+        expect(items[0].quality).to eq 47
+        expect(items[0].sell_in).to eq 8
+      end
+
+      it "A backstage pass quality increases by 3 each day when the sellIn value is below 5" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 45)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_backstage_passes()
+        expect(items[0].quality).to eq 48
+        expect(items[0].sell_in).to eq 4
+      end
+
+      it "A backstage pass quality increases by 2 each day when the sellIn value is between 10 & 5" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 9, 45)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_backstage_passes()
+        expect(items[0].quality).to eq 47
+        expect(items[0].sell_in).to eq 8
+      end
+
+      it "A backstage pass quality cannot increase past 50" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 11, 50)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_backstage_passes()
+        expect(items[0].quality).to eq 50
+        expect(items[0].sell_in).to eq 10
+      end
+
+      it "A backstage pass quality goes to 0 after its sellIn value has past zero" do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 49)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_backstage_passes()
+        expect(items[0].quality).to eq 0
+        expect(items[0].sell_in).to eq -1
+      end
+    end
+
+    describe "#update_aged_bries" do
+      it "Aged brie increases in quality by 1 and decreases Sellin by 1 after 1 day" do
+        items = [Item.new("Aged Brie", 10, 10)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_aged_bries()
+        expect(items[0].quality).to eq 11
+        expect(items[0].sell_in).to eq 9
+      end
+
+      it "Aged brie increases in quality by 2 and decreases Sellin by 2 after 2 day" do
+        items = [Item.new("Aged Brie", 10, 10)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_aged_bries()
+        shop.update_aged_bries()
+        expect(items[0].quality).to eq 12
+        expect(items[0].sell_in).to eq 8
+      end
+
+      it "Aged brie quality cannot increase beyond 50" do
+        items = [Item.new("Aged Brie", 10, 50)]
+        shop = GildedRose.new(items)
+        shop.create_categories()
+        shop.update_aged_bries()
+        expect(items[0].quality).to eq 50
+        expect(items[0].sell_in).to eq 9
+      end
+    end
+  end
